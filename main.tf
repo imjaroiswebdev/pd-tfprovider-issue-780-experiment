f825d653da07f1c01c009faf12b03e413089b1cd
José Antonio Reyes <imjaroiswebdev@gmail.com>
2024-01-12 13:40:12 -0300

    add remote backend config

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5"
 
+  backend "remote" {
+    organization = "imjarois"
+
+    workspaces {
+      name = "tfprov-issue-780"
+    }
+  }
+
   required_providers {
     pagerduty = {
       source  = "pagerduty/pagerduty"
