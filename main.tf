ae26aaca54544b5cb3eab4fc7b56db7f02be0af9
José Antonio Reyes <imjaroiswebdev@gmail.com>
2024-01-12 18:57:05 -0300

    add new tag and its assignament

--- main.tf
+++ main.tf
@@ -27,8 +27,18 @@ resource "pagerduty_tag" "issue780" {
   label = "GH-ISSUE-780"
 }
 
-resource "pagerduty_tag_assignment" "issue780_team" {
+resource "pagerduty_tag" "whatever" {
+  label = "whatever"
+}
+
+resource "pagerduty_tag_assignment" "issue780_team_tag_issue780" {
   tag_id      = pagerduty_tag.issue780.id
+  entity_type = "teams"
+  entity_id   = pagerduty_team.tfprov_issue780_team.id
+}
+
+resource "pagerduty_tag_assignment" "issue780_team_tag_whatever" {
+  tag_id      = pagerduty_tag.whatever.id
   entity_type = "teams"
   entity_id   = pagerduty_team.tfprov_issue780_team.id
 }
